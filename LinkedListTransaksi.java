03300f038c2e02ca365ee089bf96388df5548b53
rivan220804 <fadillahrivan25@gmail.com>
2025-05-30 15:17:07 +0700

    tampillinklist

--- LinkedListTransaksi.java
+++ LinkedListTransaksi.java
@@ -1,20 +1,29 @@
 public class LinkedListTransaksi {
     NodeTransaksi head;
 
-    public void tambah(TransaksiPengisian(Transaksi)){
+    public void tambah(TransaksiPengisian Transaksi){
         NodeTransaksi node = new NodeTransaksi(Transaksi);
         if (head == null) {
-            head = baru;
+            head = node;
         } else {
             NodeTransaksi temp = head;
             while (temp.next != null) {
                 temp = temp.next;
             }
-            temp.next = baru;
+            temp.next = node;
         }
     }
-
     public void tampilkan(){
-        
+        if (head == null) {
+            System.out.println("Belum ada transaksi.");
+        } else {
+            NodeTransaksi temp = head;
+            while (temp != null) {
+                temp.data.tampilkanTransaksi();
+                temp = temp.next;
+                
+                
+            }
+        }
     }
 }
