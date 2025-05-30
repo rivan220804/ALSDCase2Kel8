976a8075ea7f056b107d6f12ae10cdc80d436ed6
NaufalAbidAurizky <naufalabidau@gmail.com>
2025-05-30 14:57:54 +0700

    LinkedList

---
+++ LinkedListTransaksi.java
@@ -0,0 +1,20 @@
+public class LinkedListTransaksi {
+    NodeTransaksi head;
+
+    public void tambah(TransaksiPengisian(Transaksi)){
+        NodeTransaksi node = new NodeTransaksi(Transaksi);
+        if (head == null) {
+            head = baru;
+        } else {
+            NodeTransaksi temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = baru;
+        }
+    }
+
+    public void tampilkan(){
+        
+    }
+}
